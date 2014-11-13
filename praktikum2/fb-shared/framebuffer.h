d9fc52faaf8769e981d3bb6a7bbd6416521bfbd2
Aron Diehl <aron.diehl@flexoptix.net>
2014-11-13 11:46:36 +0100

    added shift operators

--- praktikum2/fb-shared/framebuffer.h
+++ praktikum2/fb-shared/framebuffer.h
@@ -1,7 +1,16 @@
 #pragma once
-#include <QByteArray>
+#include <QDataStream>
 
 struct FrameData {
-    quint64 bytes;
-    qint64 xres, yres, bpp;
+    quint64 xres, yres, bpp;
+
+    inline FrameData & operator<< (QDataStream & in) {
+        in  >> xres >>  yres >> bpp;
+        return * this;
+    }
+
+    inline FrameData & operator>>(QDataStream & out) {
+        out << xres << yres << bpp;
+        return * this;
+    }
 };
